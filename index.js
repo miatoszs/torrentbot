471f6a8bc6af56d681e6d6bf73244282bab21041
Áldó <73642336+miatoszs@users.noreply.github.com>
2021-11-14 21:51:32 +0100

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ fs.readdir('./events/', (err, files) => {
 	const eventHandler = require('./handler/eventHandler.js');
 	eventHandler(err, files, client);
 });
+
 fs.readdir('./commands/', (err, files) => {
 	const commandHandler = require('./handler/commandHandler.js');
 	commandHandler(err, files, client);
