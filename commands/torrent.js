db6bfccf0f51bd78bee00ad91b1249abd7036071
Áldó <73642336+miatoszs@users.noreply.github.com>
2021-11-16 21:10:57 +0100

    Help command updated

--- commands/torrent.js
+++ commands/torrent.js
@@ -8,40 +8,44 @@ module.exports = {
     description: "search torrent",
     usage: ".torrent <torrent name>",
   async execute(message, args) {
-    message.channel.send("`Searching...`")
-    let query = "";
-    args.map(string=> {
-        query += " " + string;
-    });
-    torrent_module.grabTorrents(query)
-    .then(torrentArray => {
+    try {
+      message.channel.send("`Searching...`")
+      let query = "";
+      args.map(string=> {
+          query += " " + string;
+      });
+      torrent_module.grabTorrents(query)
+      .then(torrentArray => {
 
-        if (torrentArray.length > 0)
-        {
+          if (torrentArray.length > 0)
+          {
 
-            let torrentList = new Discord.MessageEmbed();
-            torrentList.setAuthor(`@${message.author.username}`);
-            torrentArray.map((torrent) => {
-                torrentList.addFields(
-                    { name : `${torrent.number}. ${torrent.title}`, value : `${torrent.magnet} | Seeders: ${torrent.seeds} | Size: ${torrent.size} | Time: ${torrent.time}`}
-                )
-            });
-            message.channel.send(torrentList); //Send them the list of torrents in the channel
+              let torrentList = new Discord.MessageEmbed();
+              torrentList.setAuthor(`@${message.author.username}`);
+              torrentArray.map((torrent) => {
+                  torrentList.addFields(
+                      { name : `${torrent.number}. ${torrent.title}`, value : `${torrent.magnet} | Seeders: ${torrent.seeds} | Size: ${torrent.size} | Time: ${torrent.time}`}
+                  )
+              });
+              message.channel.send(torrentList); //Send them the list of torrents in the channel
 
 
-        }
-        else
-        {
-            message.channel.send(
-                new Discord.MessageEmbed()
-                    .setTitle("Not found")
-                    .setAuthor(message.author.username)
-                    .addFields(
-                        { name : "Message", value: "Torrent not found!."},
-                    )
-                    .setFooter(Date())
-            );
-        }
+          }
+          else
+          {
+              message.channel.send(
+                  new Discord.MessageEmbed()
+                      .setTitle("Not found")
+                      .setAuthor(message.author.username)
+                      .addFields(
+                          { name : "Message", value: "Torrent not found!."},
+                      )
+                      .setFooter(Date())
+              );
+          }
 
-    });
+      });
+    } catch(err) {
+      message.channel.send("There was an error executing this command!!")
+    }
   }}
